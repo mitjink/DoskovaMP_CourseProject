9f5c176248cbda7ad8923995382f5e24c85c23fe
mitjink <m.doskova06@gmail.com>
2026-06-01 13:09:16 +0300

    Fixing routes errors

--- server/src/routes/subscriptionRoutes.js
+++ server/src/routes/subscriptionRoutes.js
@@ -5,12 +5,12 @@ const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.get('/', subscriptionController.getAllPasses);
+router.get('/my', authMiddleware, subscriptionController.getMySubscriptions);
 router.get('/:id', subscriptionController.getPassById);
+
 router.post('/', authMiddleware, roleMiddleware('admin'), subscriptionController.createPass);
 router.put('/:id', authMiddleware, roleMiddleware('admin'), subscriptionController.updatePass);
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), subscriptionController.deletePass);
-
-router.get('/my', authMiddleware, subscriptionController.getMySubscriptions);
 router.post('/buy', authMiddleware, subscriptionController.buySubscription);
 
 module.exports = router;
